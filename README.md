ef6a078467cc97eaa35fe976a70a20e12f2dea33
Frank James <frank.a.james@gmail.com>
2015-02-21 22:49:18 +0000

    adding to readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # nefarious
 NFS library
+
+1. Introduction
+-----------------
+
+This is an attempt at implementing an NFS client and server in Common Lisp. 
+
+See its sister project, FRPC, for the underlying ONCRPC implementation details.
+
+2. Aims
+--------
+
+* Type in the RPC interface and handler stubs [DONE]
+* Choose a reference implementation. I've chosen FreeNFS, because it's for Windows. [DONE]
+* Successfully call client RPCs against the reference NFS implementation. I can call the various
+RPCs, but I need to add support for authentication to FRPC before I can progress further.
+* Writing the server component is probably not going to be easy.
+
+2. License
+------------
+
+Released under the terms of the MIT license.
+
+
+Frank James 
+Febuary 2015
--- handles.lisp
+++ handles.lisp
@@ -2,15 +2,43 @@
 
 (in-package #:nefarious)
 
-
-(defparameter *handles* (make-hash-table)
-  "Maps NFS handles to pathnames.")
-
 (defconstant +nefarious-handle-size+ 32)
 
-(defun make-handle (pathname)
-  (let ((h (nibbles:make-octet-vector +nefarious-handle-size+)))
-    (setf (nibbles:ub64ref/be h 0) 
-	  (sxhash pathname))
-    h))
+;; we have at least 32 bytes to play with in our file handle (possibily up to 64)
+;; what should we store in it?
+;; traditionally you would store device, inode and generation numbers
+;; but we don't really have access to these.
+;; you need a system like this so that the handle contains all the information
+;; to find the file, meaning you don't need to hold a cache mapping handles to files
+;; this is what allows the protocol to be stateless, since all the state is held by 
+;; the client in the form of handles
+;; 
 
+
+(defstruct (handle (:constructor %make-handle))
+  fh ;; nfs file handle 
+  hash ;; pathname hash
+  pathname
+  parent
+  children)
+
+(defun make-handle (pathname)
+  (let ((hash (sxhash pathname)))
+    (%make-handle 
+     :pathname pathname
+     :hash hash
+     :fh (let ((h (nibbles:make-octet-vector +nefarious-handle-size+)))
+	   (setf (nibbles:ub64ref/be h 0) 
+		 hash)
+	   h))))
+
+;; the list of top-level directories that we export i.e. that are shared
+;; using nfs
+(defparameter *exports* nil)
+
+(defun export-handles (directory)
+  (let ((handles nil))
+    (cl-fad:walk-directory directory 
+			   (lambda (pathname)
+			     (push (make-handle pathname) handles)))
+    handles))
