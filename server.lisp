9f009a88c50963b2939bf65977c7b30eb7461269
Frank James <frank.a.james@gmail.com>
2015-03-17 16:37:40 +0000

    better comments

--- server.lisp
+++ server.lisp
@@ -9,11 +9,12 @@
 (defvar *server* nil)
 
 (defun start (&key (port-mapper t))
-  "Start the NFS server."
+  "Start the NFS server. If PORT-MAPPER is nil, then it is assumed the port-mapper program is running externally to Lisp and all port mappings are added using RPC calls to it. Otherwise the server will run its own port mapper by listening on port 111, in addition to ports 635 and 2049 which are always used."
   (setf *server* (make-rpc-server))
   (let ((ports (append (when port-mapper (list port-mapper:*pmap-port*))
 		       (list *nfs-port* nfs.mount:*mount-port*))))
-    (port-mapper:add-all-mappings ports ports)
+    (port-mapper:add-all-mappings ports ports 
+                                  :rpc (not port-mapper))
     (start-rpc-server *server* 
 		      :tcp-ports ports
 		      :udp-ports ports)))
